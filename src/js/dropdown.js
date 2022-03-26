69b336361cbf8a798a9f116f2e216cc3f10957f8
spoonya <nomad8t@gmail.com>
2022-03-26 10:50:16 +0300

    fix: form dropdowm

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,41 +1,47 @@
 import { CLASSES } from './constants';
 
 export function controlDropwdown() {
-  const selected = document.querySelector('#service-selected');
-  const optionsContainer = document.querySelector('#service-container');
+  const selectedList = document.querySelectorAll(
+    '[data-form-dropdown-selected]'
+  );
 
-  const optionsList = document.querySelectorAll('.form__option');
+  selectedList.forEach((selected) => {
+    const dropdown = selected.closest('[data-form-dropdown]');
+    const optionsContainer = dropdown.querySelector(
+      '[data-form-dropdown-container]'
+    );
+    const optionsList = dropdown.querySelectorAll(
+      '[data-form-dropdown-option]'
+    );
 
-  let selectedIndex = -1;
+    let selectedIndex = -1;
 
-  selected.addEventListener('click', () => {
-    optionsContainer.classList.toggle(CLASSES.active);
-  });
-
-  optionsList.forEach((option, index) => {
-    option.addEventListener('click', () => {
-      selectedIndex = index;
-      selected.innerHTML = option.querySelector('label').innerHTML;
-      optionsContainer.classList.remove(CLASSES.active);
-      selected.classList.add(CLASSES.active);
+    optionsList.forEach((option, index) => {
+      option.addEventListener('click', () => {
+        selectedIndex = index;
+        selected.innerHTML = option.querySelector('label').textContent;
+        optionsContainer.classList.remove(CLASSES.active);
+        selected.classList.add(CLASSES.active);
+      });
     });
-  });
 
-  selected.addEventListener('click', () => {
-    selected.classList.remove(CLASSES.active);
-  });
+    selected.addEventListener('click', () => {
+      optionsContainer.classList.toggle(CLASSES.active);
+      selected.classList.remove(CLASSES.active);
+    });
 
-  window.addEventListener('click', (e) => {
-    const path = e.path || (e.composedPath && e.composedPath());
+    window.addEventListener('click', (e) => {
+      const path = e.path || (e.composedPath && e.composedPath());
 
-    if (path.includes(selected)) return;
+      if (path.includes(selected)) return;
 
-    if (optionsContainer.classList.contains(CLASSES.active)) {
-      optionsContainer.classList.remove(CLASSES.active);
-    }
+      if (optionsContainer.classList.contains(CLASSES.active)) {
+        optionsContainer.classList.remove(CLASSES.active);
+      }
 
-    if (selectedIndex !== -1) {
-      selected.classList.add(CLASSES.active);
-    }
+      if (selectedIndex !== -1) {
+        selected.classList.add(CLASSES.active);
+      }
+    });
   });
 }
